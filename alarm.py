cdf5c52521d0a518242d95fa6ba98d30c1c54d76
Fagani Hajizada <fagani.hajizada@gmail.com>
2020-12-10 20:28:52 +0100

    Improved error check function. Added support for colored text.

--- alarm.py
+++ alarm.py
@@ -1,36 +1,54 @@
 import datetime, time, random, webbrowser
 from datetime import datetime
 
+class bcolors:
+    GREEN = '\033[92m'
+    WARN = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
 current_time = time.strftime("%H:%M:%S")
 
-# Check the input from user
-def check_alarm_input(alarm_time):
-    if (0 <= alarm_minute <= 60) and (0 <= alarm_hour <= 24):
-        return True
-    else:
-        return False
-    return False
-
-# Get input from user for the alarm
-print("Please set a time for alarm. Note: 24-hour time notation.")
-while True:
-    print("")
-    print ("Currently the time is:", current_time)
-    alarm_hour = int(input("Please enter the hour (format: 'HH'): "))
-    alarm_minute = int(input("Please enter the minute (format: 'MM'): "))
-    print("")
-    try:
-        if check_alarm_input(alarm_minute):
-            break
-        if check_alarm_input(alarm_hour):
-            break
-        else:
-            raise ValueError
-    except ValueError:
-        print("ERROR: The time should have been entered in 24-hour time notation. \nMinute is between 0 and 60. Hour it is between 0 and 24.")
-
-print("The alarm has been set for:", alarm_hour, ":" ,alarm_minute)
 print("")
+print(bcolors.WARN + "Please set a time for alarm. Note: 24-hour time notation." + bcolors.ENDC)
+print (bcolors.WARN + "Currently the time is: " + current_time + bcolors.ENDC)
+print("")
+
+# Check the input from user and check if it's integer and if it's supported value
+def check_alarm_input_hour(hour):
+  while True:
+    try:
+        alarm_hour = int(input(hour))
+    except ValueError:
+       print(bcolors.FAIL + "The added value is not an integer. Please try again." + bcolors.ENDC)
+       continue
+    else:
+      if (0 <= alarm_hour <= 24): 
+        return alarm_hour
+      else:
+        print(bcolors.FAIL + "You have entered an invalid value\nThe time should have been entered in 24-hour time notation. Allowed values: between 0 and 24" + bcolors.ENDC)
+
+def check_alarm_input_minute(minute):
+  while True:
+    try:
+        alarm_minute = int(input(minute))
+    except ValueError:
+        print(bcolors.FAIL + "The added value is not an integer. Please try again." + bcolors.ENDC)
+        continue
+    else:
+      if (0 <= alarm_minute < 60):
+        return alarm_minute
+      else:
+        print(bcolors.FAIL + "You have entered an invalid value\nThe time should have been entered in 24-hour time notation. Allowed values: between 0 and 59" + bcolors.ENDC)
+
+
+alarm_hour = check_alarm_input_hour(bcolors.GREEN + "Please enter the hour (format: 'HH'): " + bcolors.ENDC)
+alarm_minute = check_alarm_input_minute(bcolors.GREEN + "Please enter the minute (format: 'MM'): " + bcolors.ENDC)
+
+print("")
+print("#########################################################################################")
+print(bcolors.WARN, "The alarm has been set for:", alarm_hour, ":" ,alarm_minute, bcolors.ENDC)
+print("#########################################################################################")
 
 # open file and read urls
 with open('video-list.txt', 'r') as video_file:
@@ -39,7 +57,7 @@ with open('video-list.txt', 'r') as video_file:
 # loop until time matches alarm time
 while True:
     if (alarm_hour == datetime.now().hour and alarm_minute == datetime.now().minute):
-        print("\nIt's time! Opening a randomly choosed video from the list in web browser")
+        print(bcolors.GREEN + "\nIt's time! Opening a randomly choosed video from the list in web browser" + bcolors.ENDC)
         randomly_selecte_video=random.choice(video_links)
         webbrowser.open(randomly_selecte_video)
         break
